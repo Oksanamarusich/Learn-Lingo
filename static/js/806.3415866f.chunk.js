9975b1e6cbd22c7ca5c87ac5492bcd8e34032cf4
Oksanamarusich <128741626+Oksanamarusich@users.noreply.github.com>
2024-03-13 18:35:09 +0000

    Deploying to gh-pages from @ Oksanamarusich/Learn-Lingo@35909a45db6499000f51d70c6003f64601946226 🚀

---
+++ static/js/806.3415866f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearnlingo=self.webpackChunklearnlingo||[]).push([[806],{806:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var l=t(184);function r(){return(0,l.jsx)("h1",{style:{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",fontSize:40,color:"#010101"},children:"Hello World"})}}}]);
+//# sourceMappingURL=806.3415866f.chunk.js.map
