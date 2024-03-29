39522a29668beec3b24d35675ceccc00523b5f9a
Oksana Marusich <oksana.m229@gmail.com>
2024-03-29 15:58:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,9 +33,7 @@ export const App = () => {
       }
     });
   }, [dispatch]);
-  return !token ?  (
-     <p>Refreshing user...</p>
-  ):(
+  return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -47,5 +45,5 @@ export const App = () => {
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  );
+  )
 };
