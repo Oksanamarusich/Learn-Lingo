1ffa7a6474feff7bd1595396582a586643fe0de8
Oksana Marusich <oksana.m229@gmail.com>
2024-04-09 21:23:47 +0300

    updata firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,12 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTO_DOMAIN,
+  databaseURL: "https://learnlingo-155f6-default-rtdb.firebaseio.com",
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  
 };
 
 export const app = initializeApp(firebaseConfig);
